b25e9cb9d45aa7b9a815423b76df0851f9db6ecd
rich <git@xpark.ca>
2024-12-25 19:46:00 -0500

    more bug fixes

---
+++ .chezmoiscripts/run_before_00-setup-omz.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+echo "Setting up Oh My Zsh structure..."
+mkdir -p "${HOME}/.oh-my-zsh/custom/themes"
+mkdir -p "${HOME}/.oh-my-zsh/custom/plugins"
+
+echo "Oh My Zsh directory structure created"
