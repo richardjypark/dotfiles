dcdca2262ce237e270e6562d27c3eefcbff36851
rich <git@richardjypark.com>
2026-02-15 10:38:20 -0500

    fix(nvim): resolve tab behavior and noice treesitter query error

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -6,3 +6,20 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+
+-- Prevent Neovim's default snippet <Tab> mapping from intercepting insert mode.
+local function force_literal_tab()
+  pcall(vim.keymap.del, "i", "<Tab>")
+  pcall(vim.keymap.del, "s", "<Tab>")
+  vim.keymap.set("i", "<Tab>", "<Tab>", { noremap = true, silent = true })
+  vim.keymap.set("s", "<Tab>", "<Tab>", { noremap = true, silent = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = force_literal_tab,
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = force_literal_tab,
+})
